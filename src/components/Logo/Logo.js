3e15de67b5e709b4082aacb42ea1840d383b7932
Yslamguly <islamguly28@gmail.com>
2022-07-27 14:59:44 +0200

    moved logo a bit down

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -4,7 +4,7 @@ import './Logo.css';
 
 const Logo = () =>{
     return(
-        <div className={'ma4 mt0'}>
+        <div className={'ma4 mt4'}>
             <Tilt className={'br2 shadow-2'} options={{ max : 45 }} style={{ height: 150, width: 150 }}>
                 <div className={'Tilt-inner'}><img src={'http://cdn.onlinewebfonts.com/svg/img_432607.png'} style={{height:150}}/>️Brain</div>
             </Tilt>
