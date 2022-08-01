66a228eb407ad690c00e8e01351438809159a3e0
Yslamguly <islamguly28@gmail.com>
2022-08-01 10:33:33 +0200

    turned states into useState hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{Component} from "react";
+import React,{useState} from "react";
 import './App.css';
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
@@ -68,16 +68,12 @@ const particlesOptions = {
 
 }
 
-class App extends Component{
-  constructor() {
-    super();
-    this.state={
-      input:'',
-      imageUrl:'',
-      box:{}
-    }
-  }
-  calculateFaceLocation = (data) =>{
+const App = () =>{
+
+  const [input,setInput] = useState('')
+  const [imageUrl,setImageUrl] = useState('')
+  const [box,setBox] = useState({});
+  const calculateFaceLocation = (data) =>{
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box
     const image = document.getElementById('inputImage')
     const width = Number(image.width)
@@ -89,34 +85,26 @@ class App extends Component{
       bottomRow : height - (clarifaiFace.bottom_row * height)
     }
   }
-  displayFaceBox = (box) =>{
-    console.log(box)
-    this.setState({box:box})
-  }
-  onInputChange = (event) =>{
-    this.setState({input:event.target.value})
-  }
-  onButtonSubmit = () =>{
-    this.setState({imageUrl:this.state.input})
+
+  const onButtonSubmit = () =>{
+    setImageUrl(input)
     app.models
         .predict(
             {
               id: "a403429f2ddf4b49b307e318f00e528b", // id of FACE_DETECT_MODEL model
               version: "34ce21a40cc24b6b96ffee54aabff139",
             },
-            this.state.input)
+            input)
         .then(response => {
-          this.displayFaceBox(this.calculateFaceLocation(response))
+          setBox(calculateFaceLocation(response))
         })
         .catch(err =>console.log(err))
   }
-  render(){
-    const particlesInit = async (main) => {
-      await loadFull(main);
-    };
-    const {imageUrl,box} = this.state
-    return (
+  const particlesInit = async (main) => {
+    await loadFull(main);
+  };
 
+    return (
       <div className="App">
         <Particles
             className={'particles'}
@@ -124,13 +112,12 @@ class App extends Component{
             init={particlesInit}
             options={particlesOptions}/>
         <Logo/>
-        <ImageLinkForm onInputChange={this.onInputChange}
-                       onButtonSubmit={this.onButtonSubmit}
+        <ImageLinkForm onInputChange={(event)=>setInput(event.target.value)}
+                       onButtonSubmit={onButtonSubmit}
         />
         <FaceRecognition box={box} imageUrl={imageUrl} />
       </div>
     );
   }
-}
 
 export default App;
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,11 +1,15 @@
 import React from "react";
 import './FaceRecognition.css'
+import Tilt from 'react-parallax-tilt';
+
 const FaceRecognition = ({imageUrl,box}) =>{
     return(
         <div className={'center ma'}>
             <div className={'absolute mt2'}>
-                <img src={imageUrl} alt={''} id={'inputImage'} width={'500px'} height={'auto'}/>
-                <div className={'bounding-box'} style={{top:box.topRow,left:box.leftCol,right:box.rightCol,bottom:box.bottomRow}}></div>
+                <Tilt>
+                    <img src={imageUrl} alt={''} id={'inputImage'} width={'500px'} height={'auto'}/>
+                    <div className={'bounding-box'} style={{top:box.topRow,left:box.leftCol,right:box.rightCol,bottom:box.bottomRow}}></div>
+                </Tilt>
             </div>
         </div>
     )
